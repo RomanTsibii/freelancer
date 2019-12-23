068ae25fccc8fdd2cd35e7a05d21c2fa54b5afd3
RomanTsibii <romanot70@gmail.com>
2019-12-23 17:41:59 +0200

    Upload User avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
   private
   def current_user_params
-    params.require(:user).permit(:from, :about, :status, :language)
+    params.require(:user).permit(:from, :about, :status, :language, :avatar)
   end
 
 end
